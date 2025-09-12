242f580a3c2860dfa87b5a303dbd5c3f7741d2df
Joshua Zingale <jtzingale@gmail.com>
2025-09-12 08:52:34 -0700

    Added TLS mode

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func main() {
 	activityFilePath := flag.String("activities", "activity-data.jsonl", "path to a .jsonl file containing the activity data.")
 	webHost := flag.String("host", "127.0.0.1", "the host for this web server")
 	port := flag.String("port", "8080", "the port for this web server")
+	useTls := flag.Bool("tls", false, "if set, uses TLS")
+	certFilePath := flag.String("certificate", "server.crt", "the TLS certificate")
+	privateKeyFilePath := flag.String("private-key", "server.key", "the TLS private key")
+
 	flag.Parse()
 
 	activityStore := store.NewActivityStore(*activityFilePath)
@@ -24,6 +28,18 @@ func main() {
 	http.HandleFunc("POST /submissions", submissionHandler.Post)
 	http.HandleFunc("OPTIONS /submissions", submissionHandler.Options)
 
-	log.Printf("Activity Grading Server running at %s:%s", *webHost, *port)
-	http.ListenAndServe(fmt.Sprintf("%s:%s", *webHost, *port), nil)
+	url := fmt.Sprintf("%s:%s", *webHost, *port)
+
+	var err error
+	if *useTls {
+		log.Printf("Starting HTTPS server at %s", url)
+		err = http.ListenAndServeTLS(url, *certFilePath, *privateKeyFilePath, nil)
+	} else {
+		log.Printf("Starting HTTP server at %s", url)
+		err = http.ListenAndServe(url, nil)
+	}
+
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
